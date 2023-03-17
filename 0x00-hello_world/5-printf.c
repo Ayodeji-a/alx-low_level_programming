306fc028edeb5f105fdb1ba620539d52294e6321
Ayodeji-a <awodelemayowasamuel@gmail.com>
2023-03-16 18:30:08 -0700

    ahah

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-
 /**
  *  main - Entry point
  *
@@ -7,12 +6,9 @@
  */
 
 int main(void)
-
 {
-
         printf("with proper grammar, but the outcome is a piece of art,\n");
        	return (0);
-
 }
 
 
