f6aa899d231c95977a26c159c3f49bb6fcc5aa46
Ayodeji-a <awodelemayowasamuel@gmail.com>
2023-03-17 18:08:28 -0700

    yg

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-    char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-    int i;
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-    for (i = 0; i < 26; i++)
-    {
+	for (i = 0; i < 26; i++)
+	{
 	    putchar(alp[i]);
-    }
-    putchar('\n');
-    return (0);
+	}
+	putchar('\n');
+	return (0);
 }
 
