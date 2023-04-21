805b5e3f5fbb2d91d53d65cac4ed9499a7ad0bed
Ayodeji-a <awodelemayowasamuel@gmail.com>
2023-04-21 13:08:14 -0700

    auk

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,32 +1,45 @@
-#include "variadic_functions.h" 
- #include <stdio.h> 
- #include <stdarg.h> 
- /** 
-  * print_strings - variadic function to print strings 
-  * @separator: const char type pointer to string between numbers 
-  * @n: const unsigned int type 
-  * Return: Always successful 
-  */ 
- void print_strings(const char *separator, const unsigned int n, ...) 
- { 
-         unsigned int i; 
-         va_list arg; 
-         char *test; 
-  
-         va_start(arg, n); 
-  
-         for (i = 0; i < n; i++) 
-         { 
-                 test = va_arg(arg, char*); 
-                 if (test) 
-                 { 
-                         printf("%s", test); 
-                 } 
-                 else 
-                         printf("(nil)"); 
-                 if (separator != NULL && (i < (n - 1))) 
-                         printf("%s", separator); 
-         } 
-         va_end(arg); 
-         printf("\n"); 
- }
+#include "variadic_functions.h"
+	#include <stdio.h>
+	#include <stdarg.h>
+
+
+	/**
+	 * print_strings - Prints strings, followed by a new line.
+	 * @separator: The string to be printed between strings.
+	 * @n: The number of strings passed to the function.
+	 * @...: A variable number of strings to be printed.
+	 *
+	 * Description: If separator is NULL, it is not printed.
+	 *              If one of the strings if NULL, (nil) is printed instead.
+	 */
+	void print_strings(const char *separator, const unsigned int n, ...)
+	{
+		va_list strings;
+		char *str;
+		unsigned int index;
+
+
+		va_start(strings, n);
+
+
+		for (index = 0; index < n; index++)
+		{
+			str = va_arg(strings, char *);
+
+
+			if (str == NULL)
+				printf("(nil)");
+			else
+				printf("%s", str);
+
+
+			if (index != (n - 1) && separator != NULL)
+				printf("%s", separator);
+		}
+
+
+		printf("\n");
+
+
+		va_end(strings);
+	}
